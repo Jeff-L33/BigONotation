7ff7a0a7d0f7848ceb5bbb4caf1c5d80c782febd
The Happy Monkey <126705970+Jeff-L33@users.noreply.github.com>
2025-03-17 08:47:45 -0600

    added bubble sort and generateted permutations

--- main.cpp
+++ main.cpp
@@ -28,8 +28,8 @@ bool binarySearch(const std::vector<int>& v, int target) {
     while (low <= high) {
         int mid = low + (high - low)/ 2;
         if (v.at(mid) == target){
-            return mid
-        }
+			return mid;
+		}
         if (v.at(mid) < target) {
             low = mid + 1;
         }
@@ -52,69 +52,91 @@ int findMax(const std::vector<int>& v) {
 }
 
 // O(n log n) - Quasi-linear Time: Merge Sort
-void merge(std::vector<int>& v, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+//void merge(std::vector<int>& v, int left, int mid, int right) {
+//    int n1 = mid - left + 1;
+//    int n2 = right - mid;
+//
+//    std::vector<int> L(n1);
+//    std::vector<int> R(n2);
+//
+//    for (int i = 0; i < n1; ++i)
+//        L[i] = v[left + i];
+//    for (int j = 0; j < n2; ++j)
+//        R[j] = v[mid + 1 + j];
+//
+//    int i = 0, j = 0, k = left;
+//    while (i < n1 && j < n2) {
+//        if (L[i] <= R[j]) {
+//            v[k] = L[i];
+//            ++i;
+//        }
+//        else {
+//            v[k] = R[j];
+//            ++j;
+//        }
+//        ++k;
+//    }
+//
+//    while (i < n1) {
+//        v[k] = L[i];
+//        ++i;
+//        ++k;
+//    }
+//
+//    while (j < n2) {
+//        v[k] = R[j];
+//        ++j;
+//        ++k;
+//    }
+//}
 
-    std::vector<int> L(n1);
-    std::vector<int> R(n2);
-
-    for (int i = 0; i < n1; ++i)
-        L[i] = v[left + i];
-    for (int j = 0; j < n2; ++j)
-        R[j] = v[mid + 1 + j];
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            v[k] = L[i];
-            ++i;
-        }
-        else {
-            v[k] = R[j];
-            ++j;
-        }
-        ++k;
-    }
-
-    while (i < n1) {
-        v[k] = L[i];
-        ++i;
-        ++k;
-    }
-
-    while (j < n2) {
-        v[k] = R[j];
-        ++j;
-        ++k;
-    }
-}
-
-void mergeSort(std::vector<int>& v, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(v, left, mid);
-        mergeSort(v, mid + 1, right);
-
-        merge(v, left, mid, right);
-    }
-}
+//void mergeSort(std::vector<int>& v, int left, int right) {
+//    if (left < right) {
+//        int mid = left + (right - left) / 2;
+//
+//        mergeSort(v, left, mid);
+//        mergeSort(v, mid + 1, right);
+//
+//        merge(v, left, mid, right);
+//    }
+//}
 
 // O(n^2) - Quadratic Time: Bubble Sort
 void bubbleSort(std::vector<int>& v) {
-
+    bool swapped;
+    for (int outer = 0; outer < v.size() - 1; ++outer) { // n- 1
+		swapped = false;
+		for (int inner = 0; inner < v.size() - outer - 1; ++inner) {
+			if (v[inner] > v[inner + 1]) {
+				std::swap(v[inner], v[inner + 1]);
+				swapped = true;
+			}// n - 2
+		}// n squared
+		if (!swapped) {
+			break;
+		}
+   }
 }
 
 // O(2^n) - Exponential Time: Fibonacci using Recursion
-int fibonacci(int n) {
-    if (max <= 1) return max;
-    
-    return fibonacci(max - 1) + fibonacci(max - 2);
-}
+//int fibonacci(int n) {
+//    if (max <= 1) return max;
+//    
+//    return fibonacci(max - 1) + fibonacci(max - 2);
+//}
 
 // O(n!) - Factorial Time: Generating all permutations of a vector
-void generatePermutations(std::vector<int>& v, int l, int r) {
+void generatePermutations(std::vector<int>& v, int l, int r, std::vector<std::vector<int>> & results) {
+	// Base case: l == r, we permutate
+	if (l == r) {
+		results.push_back(v);
+        return;
+	}
+	for (int i = l; i <= r; ++i) {
+		std::swap(v[l], v[i]);
+		generatePermutations(v, l + 1, r, results);
+		std::swap(v[l], v[i]);
+	}
 
 }
 
@@ -123,7 +145,7 @@ int main() {
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dist(INT_MIN, INT_MAX);
 
-    std::vector<int> v(399999999);
+    std::vector<int> v(1001);
     for (int& num : v) {
         num = dist(gen); // Random numbers in the full integer range
     }
@@ -140,11 +162,11 @@ int main() {
 
     std::cout << "O(n): Max Element time: " << timeFunction(findMax, v) << " ms" << std::endl;
 
-    std::cout << "O(n log n): Merge Sorting time: " << timeFunction(mergeSort, v) << " ms" << std::endl;
+    //std::cout << "O(n log n): Merge Sorting time: " << timeFunction(mergeSort, v) << " ms" << std::endl;
 
-    // std::cout << "O(n^2): Bubble Sorting time: " << timeFunction(bubbleSort, v) << " ms" << std::endl;
+    std::cout << "O(n^2): Bubble Sorting time: " << timeFunction(bubbleSort, v) << " ms" << std::endl;
     
-    std::cout << "O(2^n): Fibonacci(5) time: " << timeFunction(fibonacci, 5) << " ms" << std::endl;
+    //std::cout << "O(2^n): Fibonacci(5) time: " << timeFunction(fibonacci, 5) << " ms" << std::endl;
     
     // std::cout << "O(n!): Generating Permutations (first 6 elements) time: " << timeFunction(generatePermutations, v, 0, 5) << " ms" << std::endl;
     
